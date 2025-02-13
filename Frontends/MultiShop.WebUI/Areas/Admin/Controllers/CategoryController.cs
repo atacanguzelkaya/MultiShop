1efb9a7a820d121b1f717677c60dded575ab974e
atacanguzelkaya <atacangzlky@gmail.com>
2025-02-13 20:15:57 +0300

    Added AdminLayout

---
+++ Frontends/MultiShop.WebUI/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MultiShop.WebUI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            CategoryViewbagList();
+            return View();
+        }
+        void CategoryViewbagList()
+        {
+            ViewBag.v1 = "Ana Sayfa";
+            ViewBag.v2 = "Kategoriler";
+            ViewBag.v3 = "Kategori Listesi";
+            ViewBag.v0 = "Kategori İşlemleri";
+        }
+    }
+}
