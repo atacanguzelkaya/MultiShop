4f5355e78c5a3f767cb87a757b82092a16faa4a2
atacanguzelkaya <atacangzlky@gmail.com>
2025-03-08 15:59:42 +0300

    Added GoogleCloudStorage and Mailkit

---
+++ Services/Images/MultiShop.ImageUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MultiShop.ImageUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
